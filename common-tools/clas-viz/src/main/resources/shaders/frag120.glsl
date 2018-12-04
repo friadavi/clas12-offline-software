ddb385d094a093a1a7bf39a6ce15df55193565a0
David Friant <friadavi@isu.edu>
2018-12-04 16:40:55 -0500

    Bring the package naming convention into line with the rest of the project.

---
+++ common-tools/clas-viz/src/main/resources/shaders/frag120.glsl
@@ -0,0 +1,9 @@
+#version 120 core
+
+uniform vec4 clrVec;
+
+
+void main()
+{
+	gl_FragColor = clrVec;
+}
---
+++ common-tools/clas-viz/src/main/resources/shaders/vert120.glsl
@@ -0,0 +1,10 @@
+#version 120 core
+
+//layout (location = 0) in vec4 vert;
+
+uniform  mat4 mvpMat;
+
+void main()
+{
+	gl_Position = mvpMat * vert;
+}
